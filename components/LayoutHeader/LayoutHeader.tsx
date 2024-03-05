1d5fc4fc8a6a4a591fb8c4d7c095d44df19ffdc9
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-03-05 13:40:09 -0800

    style header for mobile

--- components/LayoutHeader/LayoutHeader.tsx
+++ components/LayoutHeader/LayoutHeader.tsx
@@ -1,4 +1,4 @@
-import { Box, Burger } from '@mantine/core';
+import { Box, Burger, Button, Flex, Image, Text } from '@mantine/core';
 import styles from './LayoutHeader.module.css';
 
 interface LayoutHeaderProps {
@@ -10,13 +10,20 @@ const LayoutHeader = ({opened, toggle}:LayoutHeaderProps) => {
 
   return (
     <Box className={styles.layoutHeader}>
-      <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
-        <div>Logo</div>
+      <Flex className={styles.layoutHeaderLeft}>
+        <Image h={24} w={24} src={'/assets/logo-mobile.svg'} />
+        <Flex className={styles.headerTitleContainer} onClick={toggle}>
+          <Text>Platform Launch</Text>
+          <Image h={6} w={12} src={'/assets/icon-chevron-down.svg'} />
+        </Flex>
+      </Flex>
+
+      <Flex className={styles.layoutHeaderRight}>
+        <Button bg={'#635FC7'} h={32} w={48} radius={24}>
+          <Text style={{fontWeight: 'bold', fontSize: '18px'}}>+</Text>
+        </Button>
+        <Image className={styles.headerElipses} h={16} src={'/assets/icon-vertical-ellipsis.svg'} />
+      </Flex>
     </Box>
   );
 }
