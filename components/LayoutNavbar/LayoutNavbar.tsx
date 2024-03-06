787e261e4c6e554ce68e64824e990e40c524842f
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-03-05 17:31:21 -0800

    styled nav

--- components/LayoutNavbar/LayoutNavbar.tsx
+++ components/LayoutNavbar/LayoutNavbar.tsx
@@ -1,16 +1,50 @@
-import { Box } from "@mantine/core";
+import { Flex, Image, Switch, Text } from "@mantine/core";
 import styles from './LayoutNavbar.module.css';
-
-interface LayoutNavbarProps {
-  
-}
+import { useState } from "react";
+import cx from 'clsx';
 
 const LayoutNavbar = ({}) => {
+  const [selectedBoard, setSelectedBoard] = useState<string | null>('Platform Launch');
+  const numberOfBoards = 3;
+  const boardsList = ['Platform Launch', 'Marketing Plan', 'Roadmap']
+
+  const handleBoardClick = (board: string) => {
+    setSelectedBoard(board);
+  };  
   
   return (
-    <Box className={styles.layoutNav}>
-      Navbar
-    </Box>
+    <Flex className={styles.layoutNav}>
+      <Flex className={styles.navbarTop}>
+        <Text className={styles.navbarHeaderText}>All Boards ({numberOfBoards})</Text>
+        {boardsList.map((board, index) => (
+          <Flex
+            className={cx(styles.navbarBoardsTabs, { [styles.selected]: board === selectedBoard })}
+            key={board}
+            onClick={() => handleBoardClick(board)}
+
+          >
+            <Image className={styles.icon} h={16} w={16} src={'/assets/icon-board.svg'} />
+            <Text>{board}</Text>
+          </Flex>
+        ))}
+        <Flex className={styles.navbarCreateBoard}>
+          <Image h={16} w={16} src={'/assets/icon-board.svg'} />
+          <Text className={styles.navbarCreateBoardText}>+ Create New Board</Text>
+        </Flex>
+      </Flex>
+
+      <Flex className={styles.navbarBottom}>
+        <Flex className={styles.navbarThemeToggle}>
+          <Image h={16} w={16} src={'/assets/icon-light-theme.svg'} />
+          <Switch defaultChecked />
+          <Image h={16} w={16} src={'/assets/icon-dark-theme.svg'} />
+        </Flex>
+        <Flex className={styles.hideSidebar}>
+          <Image h={16} w={16} src={'/assets/icon-hide-sidebar.svg'} />
+          <Text className={styles.hideSidebarText}>Hide Sidebar</Text>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
 
