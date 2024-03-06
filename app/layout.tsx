8d8cb669d4a1d646ac330398cf733fd3df601ca6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-03-06 11:17:16 -0800

    update style and add server-only for testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import '@mantine/core/styles.css';
+import'./global.css'
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core'
 import Layout from "@/components/Layout/Layout";
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Title } from "@mantine/core";
 import styles from "./page.module.css";
+import 'server-only'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      Server Component
+      <Title>Server Component</Title>
     </main>
   );
 }
