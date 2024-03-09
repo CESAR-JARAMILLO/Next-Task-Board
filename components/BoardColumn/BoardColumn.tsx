a2b8dae5e40c4ad0b332e050cdae2226094d7ec3
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2024-03-09 00:06:03 -0800

    style taskTypeText

--- components/BoardColumn/BoardColumn.tsx
+++ components/BoardColumn/BoardColumn.tsx
@@ -12,6 +12,19 @@ const BoardColumn = ({ taskType }: BoardColumnProps) => {
 
   const column = data.boards[0].columns.find(column => column.name === taskType);
 
+  const taskTypeCircleClass = () => {
+    switch(taskType) {
+      case 'Todo':
+        return styles.todoTaskTypeCircle;
+      case 'Doing':
+        return styles.doingTaskTypeCircle;
+      case 'Done':
+        return styles.doneTaskTypeCircle;
+      default:
+        return styles.taskTypeCircle;
+    }
+  };
+
 
   if (!column) {
     return <div>Column not found</div>;
@@ -22,7 +35,7 @@ const BoardColumn = ({ taskType }: BoardColumnProps) => {
       <Flex className={styles.boardColumn}>
         <Box className={styles.columnContent}>
           <Flex className={styles.columnTaskType}>
-            <Box className={styles.taskTypeCircle} />
+            <Box className={taskTypeCircleClass()} />
             <Text className={styles.taskTypeText}>{`${taskType} (${column.tasks.length})`}</Text>
           </Flex>
           {column.tasks.map((task, index) => (
